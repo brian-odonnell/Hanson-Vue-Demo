5e201c29d233e3be10d07e863cbfd97f69247887
Brian O'Donnell <brian.odonnell@hansoninc.com>
2022-08-29 16:17:37 -0400

    Rebuilt and added note about serving JSON data in readme file

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# Hanson-Vue-Training
+# dojo-blog
+
+## Project setup
+```
+npm install
+```
+
+## Serve JSON data
+```
+json-server --watch data/db.json
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/views/DetailsView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div v-if="error">{{ error }}</div>
+    <div v-if="post" class="post">
+      <h3>{{ post.title }}</h3>
+      <p class="pre">{{ post.body }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import getPost from '../composables/getPost';
+
+export default {
+  props: ['id'],
+  setup(props) {
+    const { post, error, load } = getPost(props.id);
+
+    load();
+
+    return { post, error };
+  }
+}
+</script>
+
+<style>
+  .post {
+    max-width: 1200px;
+    margin: 0 auto;
+  }
+</style>
